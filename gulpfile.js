96775df30469a19baedc0eb8fc625c7840d2bdb5
andrey.antropov <andreyantropov.1994@gmail.com>
2024-07-15 10:07:20 +0500

    gh-pages

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const { series, parallel } = require('gulp');
-const buildTasks = require('./gulp-tasks/gulpfile.build.js');
-const devTasks = require('./gulp-tasks/gulpfile.dev.js');
-
-const build = series(buildTasks.clean, parallel(buildTasks.scripts, buildTasks.styles, buildTasks.resources, buildTasks.images, buildTasks.svgSprites, buildTasks.fonts, buildTasks.htmlMinify));
-exports.build = build;
-
-const dev = series(devTasks.clean, parallel(devTasks.scripts, devTasks.styles, devTasks.resources, devTasks.images, devTasks.svgSprites, devTasks.fonts, devTasks.html), devTasks.devServer);
-exports.dev = dev;
